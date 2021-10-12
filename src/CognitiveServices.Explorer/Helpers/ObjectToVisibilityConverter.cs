e0596d7536e30170c10ecf0fd49f0ba3b3557b48
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2021-10-12 10:42:52 +0200

    Improving : Design on picture analysis page.

---
+++ src/CognitiveServices.Explorer/Helpers/ObjectToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace CognitiveServices.Explorer.Helpers
+{
+    public class ObjectToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value != null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/CognitiveServices.Explorer/Views/PictureAnalysePage.xaml.cs
+++ src/CognitiveServices.Explorer/Views/PictureAnalysePage.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using CognitiveServices.Explorer.ViewModels;
 
 using CommunityToolkit.Mvvm.DependencyInjection;
-
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
 
 namespace CognitiveServices.Explorer.Views
 {
@@ -14,6 +15,11 @@ namespace CognitiveServices.Explorer.Views
         {
             ViewModel = Ioc.Default.GetService<PictureAnalyseViewModel>();
             InitializeComponent();
+        }
+
+        private void PictureImage_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            FlyoutMenu?.ShowAt(PictureImage, new() { Position = new(PictureImage.ActualWidth / 2, 50), ShowMode = FlyoutShowMode.Transient });
         }
     }
 }
